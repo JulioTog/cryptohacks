02edcc779de97e092a61b3410a9e734e2f13cd1a
JulioTog <julio@tognetti.ar>
2024-01-18 23:03:04 -0300

    refactor xor func

--- internals/decoders.go
+++ internals/decoders.go
@@ -23,21 +23,7 @@ func AsciiDecoder(stringToDecode []byte) string {
 	return decodedString
 }
 
-func LongNumDecoder(stringToDecode big.Int) string{
+func LongNumDecoder(stringToDecode big.Int) string {
 	b := stringToDecode.Bytes()
 	return AsciiDecoder(b)
-}
-
-func XorCalc (b1,b2 []byte)  (result []byte){
-	if len(b1) != len(b2){
-		fmt.Println("Not same lenght")
-		result = []byte(nil) 
-		return 
-	}else{
-	for i,v := range b1{
-		key := v ^ b2[i]
-		result = append(result,key)
-	}	
-	}
-	return
 }
---
+++ internals/logicalOperation.go
@@ -0,0 +1,17 @@
+package internals
+
+import "fmt"
+
+func XorCalc(b1, b2 []byte) (result []byte) {
+	if len(b1) != len(b2) {
+		fmt.Println("Not same lenght")
+		result = []byte(nil)
+		return
+	} else {
+		for i, v := range b1 {
+			key := v ^ b2[i]
+			result = append(result, key)
+		}
+	}
+	return
+}
