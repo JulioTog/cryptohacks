9eb16a223bd264bc1161d78e32a516c366d79a4c
JulioTog <julio@tognetti.ar>
2024-01-19 09:11:31 -0300

    xorkey1 exercise

---
+++ cmd/xorkey1/xorkey1.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"JulioTog/CryptohacksCode/internals"
+	"encoding/hex"
+	"fmt"
+	"math"
+)
+
+var hexMessage = "0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104"
+var alwaysInit = []byte("crypto{")
+
+func main() {
+	b1, _ := hex.DecodeString(hexMessage)
+
+	normalizedByte := make([]byte, len(b1))
+	copy(normalizedByte, alwaysInit)
+	result := internals.XorCalc(b1, normalizedByte)
+	fmt.Println(internals.AsciiDecoder(result[:7]))
+
+	key := result[:7]
+	key = append(key, 'y')
+	key2 := make([]byte, len(b1))
+	for i, _ := range b1 {
+		key2[i] = key[int(math.Mod(float64(i), float64(len(key))))]
+	}
+
+	message := internals.XorCalc(key2, b1)
+	fmt.Println(internals.AsciiDecoder(message))
+}
